597f2ab7903978dcbc7995eec725929e894bd30f
Johan <29obituary@gmail.com>
2022-02-07 23:19:04 -0300

    List client and get client by id

--- src/main/java/com/ventas/ventas/controller/ClienteController.java
+++ src/main/java/com/ventas/ventas/controller/ClienteController.java
@@ -5,6 +5,7 @@ import com.ventas.ventas.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,13 @@ public class ClienteController {
     public ResponseEntity<List<Cliente>> findAll(){
         return ResponseEntity.ok(clienteService.findAll());
 
+    }
+
+    @GetMapping("/{id}")
+    public  ResponseEntity<Cliente> findById(@PathVariable("id") Integer idCliente){
+        return clienteService.findById(idCliente)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 }
 
